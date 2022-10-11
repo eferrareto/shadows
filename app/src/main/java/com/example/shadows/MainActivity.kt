01e13c267d4fa4ee46bf259741a180baa17d8337
Erick Ferrareto <e.ferraretowork@gmail.com>
2022-10-10 22:25:44 -0300

    working with shadows

---
+++ app/src/main/java/com/example/shadows/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.shadows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+                              
+
+    }
+}
